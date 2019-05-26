1558b9a28c0b13cb959f8a6d1a49def66cc77e04
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-05-26 18:08:47 +0200

    clearing up travel page

--- routes/views/travels.js
+++ routes/views/travels.js
@@ -7,72 +7,30 @@ exports = module.exports = function (req, res) {
 	var locals = res.locals;
 
 	// Init locals
-	locals.section = 'events';
+	locals.section = 'Travels';
+
 	locals.filters = {
-		category: req.params.category,
+		destination: req.params.category,
 	};
+
 	locals.data = {
-		posts: [],
-		categories: [],
+		travels: [],
+		destinations: [],
 	};
 
 	// Load all categories
 	view.on('init', function (next) {
-
-		keystone.list('PostCategory').model.find().sort('name').exec(function (err, results) {
-
-			locals.data.categories = results;
-
-			// Load the counts for each category
-			async.each(locals.data.categories, function (category, next) {
-
-				keystone.list('Post').model.count().where('categories').in([category.id]).exec(function (err, count) {
-					category.postCount = count;
-					next(err);
-				});
-
-			}, function (err) {
-				next(err);
-			});
+		keystone.list('Travel').model.find().sort('name').exec(function (err, results) {
+			console.log(results);
+			locals.data.travels = results;
+			next()
 		});
 	});
 
 	// Load the current category filter
-	view.on('init', function (next) {
+	// view.on('init', function (next) {
 
-		if (req.params.category) {
-			keystone.list('PostCategory').model.findOne({ key: locals.filters.category }).exec(function (err, result) {
-				locals.data.category = result;
-				next(err);
-			});
-		} else {
-			next();
-		}
-	});
-
-	// Load the posts
-	view.on('init', function (next) {
-
-		var q = keystone.list('Post').paginate({
-			page: req.query.page || 1,
-			perPage: 10,
-			maxPages: 10,
-			filters: {
-				state: 'published',
-			},
-		})
-			.sort('-publishedDate')
-			.populate('author categories');
-
-		if (locals.data.category) {
-			q.where('categories').in([locals.data.category]);
-		}
-
-		q.exec(function (err, results) {
-			locals.data.posts = results;
-			next(err);
-		});
-	});
+	// });
 
 	// Render the view
 	view.render('travels');
