8151c95fa53f4f30f48a47ee08c9e1d753adec99
JakobKilias <jakobkilias0311@gmail.com>
2019-05-27 13:50:45 +0200

    gallery

--- routes/views/gallery.js
+++ routes/views/gallery.js
@@ -7,9 +7,22 @@ exports = module.exports = function (req, res) {
 
 	// Set locals
 	locals.section = 'gallery';
+	
+	locals.images = [];
 
 	// Load the galleries by sortOrder
 	view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
+
+	view.on('init', function(next) {
+		keystone.list('Travel').model.find().exec((err, result) => {
+			result.map(travel => {
+				locals.images.push(travel.image);
+			});
+			next(err);
+		});
+
+
+	});
 
 	// Render the view
 	view.render('gallery');
